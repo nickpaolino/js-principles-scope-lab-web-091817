04dddb0c1e26614db813f1a5994818004ec5cadc
Nick Paolino <github email address>
2017-10-29 15:28:42 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your solution in this file!
+customerName = "bob";
+
+function customerName(){
+  return customerName;
+}
+
+function upperCaseCustomerName(){
+  customerName = customerName.toUpperCase();
+}
+
+var bestCustomer;
+
+function setBestCustomer(){
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+  bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = "B";
+
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = "Bob";
+}
+
+let favoriteCustomer = "Nick";
+
+function attemptTwoFavoriteCustomers(){
+  let favoriteCustomer = "Jeremy";
+  let favoriteCustomer = "Jeremy";
+}
